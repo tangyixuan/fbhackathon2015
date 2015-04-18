cba26807acc16a2eb2195d047d7b688ee3ee0a5a
tangyixuan <ayixuan@hotmail.com>
2015-04-18 18:14:27 +1000

    new project

---
+++ BikingTogether/BikingTogether/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BikingTogether
+//
+//  Created by tangyixuan on 18/4/15.
+//  Copyright (c) 2015 Hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BikingTogether/BikingTogether/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BikingTogether
+//
+//  Created by tangyixuan on 18/4/15.
+//  Copyright (c) 2015 Hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
